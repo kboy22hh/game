c71c76c6dbae370f239e4573477deb4acc2b8dd3
kboy22hh <112634983+kboy22hh@users.noreply.github.com>
2024-10-23 17:34:11 -0400

    improved player movement

--- Game/Assets/player1.cs
+++ Game/Assets/player1.cs
@@ -4,49 +4,50 @@ using UnityEngine;
 
 public class player1 : MonoBehaviour
 {
-
-    Rigidbody2D rb;
-    public float movementspeed;
-    public float height;
-    private KeyCode lastHitKey;
+    
+    [SerializeField] float moveSpeed;
+    [SerializeField] float jumpForce;
 
    
-    // Start is called before the first frame update
+    private Rigidbody2D rb;
+
+    
     void Start()
     {
         
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        if(Input.GetKeyDown (KeyCode.Space))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, height);
-        }
 
-        if(Input.GetKeyDown (KeyCode.LeftArrow))
-        {
-            if(lastHitKey == KeyCode.LeftArrow) {
+        Move();
 
-        }else
-            {
-                GetComponent<Rigidbody2D>().velocity = new Vector2 ((movementspeed - movementspeed * 2), 0);
-                lastHitKey = KeyCode.LeftArrow;
-            }
-    }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+      
+        if (Input.GetButtonDown("Jump"))
         {
-            if (lastHitKey == KeyCode.RightArrow)
-            {
-
-            }
-            else
-            {
-                GetComponent<Rigidbody2D>().velocity = new Vector2((movementspeed - movementspeed * 2), 0);
-                lastHitKey = KeyCode.RightArrow;
-            }
+            Jump();
         }
     }
 
+    
+    private void Move()
+    {
+        
+        float moveInput = Input.GetAxis("Horizontal");
+
+       
+        transform.Translate(moveInput * Vector2.right * moveSpeed * Time.deltaTime);
+        
+    }
+
+ 
+    private void Jump()
+    {
+        
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
 }
+
+
