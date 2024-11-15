92924e62f7b8ae5015e9235bf443d4b904c11445
kboy22hh <112634983+kboy22hh@users.noreply.github.com>
2024-11-15 11:56:58 -0500

    do not let player1 go through red barriiers

--- Game/Assets/door 1.cs
+++ Game/Assets/door 1.cs
@@ -5,9 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class door1 : MonoBehaviour
 {
+
+    Vector3 originalPos;
     void Awake()
     {
         GetComponent<Collider2D>().isTrigger = true;
+        originalPos = new Vector3((float)-6.9088, (float)-3.0436, (float)0);
     }
 
     private void OnTriggerEnter2D(Collider2D c2d)
@@ -15,8 +18,15 @@ public class door1 : MonoBehaviour
         if (c2d.CompareTag("Player2"))
         {
             Destroy(gameObject);
+            GetComponent<Collider2D>().isTrigger = true;
         }
-        
+        if (c2d.CompareTag("Player"))
+        {
+            c2d.transform.position = originalPos;
+
+
+        }
+ 
     }
 
  
